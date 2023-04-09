95d368a7e06ba3b9347b461b29474329d63f6705
thiago-anjos <your_email@example.com>
2023-04-09 00:11:50 -0300

    criando um evento de escuta no dom com o HotListener

--- src/app/calendar/paint.directive.ts
+++ src/app/calendar/paint.directive.ts
@@ -1,15 +1,19 @@
-import { Directive, OnInit, ElementRef, Input } from '@angular/core';
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appPaint]',
 })
-export class PaintDirective implements OnInit {
+export class PaintDirective {
   constructor(private elementRef: ElementRef) {}
 
   @Input()
   color: string = 'Yellow';
 
-  ngOnInit(): void {
-    this.elementRef.nativeElement.style.backgroundColor = this.color;
+  @HostListener('mouseover') onMouseOver() {
+    this.elementRef.nativeElement.style.backgroundColor = 'green';
+  }
+
+  @HostListener('mouseout') onMouseOut() {
+    this.elementRef.nativeElement.style.backgroundColor = 'pink';
   }
 }
